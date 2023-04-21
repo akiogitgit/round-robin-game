cc3247e58bc944ff3e10a35b72642ead5de57989
akiogitgit <akimaron2828@icloud.com>
2023-04-22 00:09:18 +0900

    feat: Boardコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -9,40 +9,12 @@ export default function App() {
       <h1 className="text-2xl">○✕ゲーム</h1>
 
       <div className="mt-4">
-        {fields.map((_, index1) => (
-          <div className="flex" key={index1}>
-            {_.map((value, index2) => (
-              <button
-                // 押されているものはホバー、フォーカスのスタイルが当たらない
-                // ただし押してから3秒間はフォーカスのスタイルが当たる
-                className={`border rounded-lg border-stone-300 h-20 text-30px w-20 duration-200 ${
-                  !value && !winner && "hover:(border-stone-100 bg-stone-700)"
-                } ${
-                  !winner &&
-                  (currentField.join(" ") === `${index1} ${index2}` || !value) &&
-                  "focus:(ring-3 ring-stone-100)"
-                }`}
-                tabIndex={value || winner ? -1 : 1}
-                onClick={() => onClickField(index1, index2)}
-                key={index2}
-              >
-                {value}
-              </button>
-            ))}
-          </div>
-        ))}
+        <Board fields={fields} onClickField={onClickField} currentField={currentField} winner={winner} />
       </div>
 
       {winner && <p className="mt-4 text-2xl">{winner}の勝ち</p>}
 
-      <button
-        className="mt-4"
-        onClick={() => {
-          setFields([...Array(FIELD_SIZE)].map((_) => [...Array(FIELD_SIZE)].map((_) => null)) as Fields);
-          setPlayer("✕");
-          setWinner(null);
-        }}
-      >
+      <button className="mt-4" onClick={reset}>
         リセット
       </button>
     </div>
