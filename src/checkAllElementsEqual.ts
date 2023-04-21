0e45d5ebcae01da5c92b6ef16616999485ca6631
akiogitgit <akimaron2828@icloud.com>
2023-04-22 06:21:42 +0900

    chore: コメント追加

--- src/checkAllElementsEqual.ts
+++ src/checkAllElementsEqual.ts
@@ -1,6 +1,7 @@
+// 配列の要素が全て同じ
 export const checkAllElementsEqual = (array: readonly any[]) => {
   // 配列の重複を無くす
   const target = [...array].filter((v, i) => i === array.findLastIndex((v2) => v2 === v));
 
-  return target.length === 1; // 全て同じ要素なら長さは1になる
+  return target.length === 1;
 };
--- src/tuple2D.d.ts
+++ src/tuple2D.d.ts
@@ -1,3 +1,4 @@
+// 例）Tuple2D<Field, 3> => [[Field,Field,Field],[Field,Field,Field],[Field,Field,Field]]
 export type Tuple2D<T, S extends number, A1 extends T[] = [], A2 extends T[][] = []> = A2["length"] extends S
   ? A2
   : A1["length"] extends S
